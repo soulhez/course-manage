ba21c98f0bd05ede9fa73f395aa36970fe91b3b9
yhl221 <1727538058@qq.com>
2018-04-07 16:15:57 +0800

    完成注册页面展示

---
+++ public/js/components/register-component.js
@@ -0,0 +1,30 @@
+import React, {Component} from "react";
+ class Register extends Component{
+     render(){
+         return  <div className="login-container">
+             <div className="logo">
+                 西安邮电大学
+             </div>
+             <div className="login-putin" classID="isLogin">
+                 <div className="tab-body">
+                     <div className="input-container">
+                         <input type="text" id="username" className="input-style " placeholder="用户名"/>
+                     </div>
+                     <div className="input-container">
+                         <input type="password" id="password" placeholder="密码" className="input-style"/>
+                     </div>
+                     <div className="input-container">
+                         <input type="password" id="password" placeholder="确认密码" className="input-style"/>
+                     </div>
+                     <form className="identity">
+                         <input type="radio" value="学生" name="identity"/><span className="radio-position">学生</span>
+                         <input type="radio" value="教师" name="identity"/><span className="radio-position">教师</span>
+                     </form>
+                     <input type="submit" value="登录" className="submit-button"/>
+                 </div>
+             </div>
+         </div>
+     }
+ }
+
+ export default Register;
