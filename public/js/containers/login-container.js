3d656ca2ee580a03fcf947b276d04930e419fad9
yhl221 <1727538058@qq.com>
2018-04-07 09:42:13 +0800

    完成登录组件页面展示

---
+++ public/js/containers/login-container.js
@@ -0,0 +1,4 @@
+import Login from "../components/login-component";
+import {connect} from "react-redux";
+
+export default connect()(Login);
--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {render} from "react-dom";
 import {Provider} from "react-redux";
 import {Router, Route, browserHistory} from 'react-router';
-import Home from "./containers/home"
+import Login from "./containers/login-container"
 import {createStore, applyMiddleware} from 'redux';
 import reducer from "./reducers/index"
 
@@ -14,7 +14,7 @@ const store = createStoreWithMiddleware(reducer);
 render(
     <Provider store={store}>
         <Router history={browserHistory}>
-            <Route path="/" component={Home}/>
+            <Route path="/" component={Login}/>
         </Router>
     </Provider>, document.getElementById('app'));
 
