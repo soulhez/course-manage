c6aac00d346f4e18efe39bd59e070b5da9dc758a
yhl221 <1727538058@qq.com>
2018-04-18 10:33:06 +0800

    完成修改前端动态页面展示，表格内不可编辑问题还未解决

--- public/js/components/userManage-component.js
+++ public/js/components/userManage-component.js
@@ -10,8 +10,36 @@ class UserManage extends Component {
         this.props.onRemove({id:user_id});
     }
 
+    modifyUser(id,password){
+       /* $(this).attr("type","text");*/
+
+        let style=`glyphicon glyphicon-pencil modify_color`;
+        console.log($(`input[id=${id}]`).val());
+        $(`input[id=${id}]`).removeAttr("disabled");
+        $(`input[id=${id}]`).addClass("border_style");
+        $('tr span:nth-child(2)').attr("class","hidden_flag");
+        $('tr span:nth-child(3)').attr("class","glyphicon glyphicon-remove remove_color");
+        $('tr span:nth-child(4)').attr("class","glyphicon glyphicon-ok right_color");
+        $('tr span:nth-child(3)').on("click",()=>{
+            //取消修改
+            console.log('cancel');
+            $(`input[id=${id}]`).attr("class","remove_border");
+            $(`input[id=${id}]`).attr("disabled","disabled");
+            $(`input[id=${id}]`).attr("value",`${password}`);
+        });
+        $('tr span:nth-child(4)').on("click",()=>{
+             //提交修改
+            let new_password= $(`input[id=${id}]`).val();
+            console.log(new_password);
+           // this.props.onModifyUser({password});
+        });
+
+       // $(this).hasClass(`${style}`).siblings(`.${style}`).removeClass(`${style}`);
+        // $(this).siblings(`.${style}`).removeClass(`${style}`);
+
+    }
+
     componentDidUpdate(){
-        console.log(this.props.allUsers);
         this.props.getAllUsers();
     }
 
@@ -24,25 +52,27 @@ class UserManage extends Component {
                     <table className="table table-bordered">
                         <thead>
                         <tr className="active">
-                            <th className="col-md-1">ID</th>
-                            <th className="col-md-1">姓名</th>
-                            <th className="col-md-1">密码</th>
-                            <th className="col-md-1">身份</th>
-                            <th className="col-md-1">操作</th>
+                            <th className="col-md-1 text_type">ID</th>
+                            <th className="col-md-1 text_type">姓名</th>
+                            <th className="col-md-1 text_type">密码</th>
+                            <th className="col-md-1 text_type">身份</th>
+                            <th className="col-md-1 text_type">操作</th>
                         </tr>
                         </thead>
                         <tbody>
                         {this.props.allUsers.map((element,index)=>{
-                            return <tr key={index}>
+                            return <tr key={index} className="text_type">
                                 <td>{element.id}</td>
                                 <td>{element.name}</td>
                                 <td>
-                                    <input type="text" disabled="disabled" value={element.password} className="remove_border"/>
-                                    <span className="glyphicon glyphicon-pencil pencil_color"></span>
+                                    <input value={element.password} className="remove_border" id={element.id} disabled="disabled"/>
+                                    <span className="glyphicon glyphicon-pencil modify_color" onClick={this.modifyUser.bind(this,element.id)} id={element.id}></span>
+                                    <span></span>
+                                    <span></span>
                                     </td>
                                 <td>{element.identity}</td>
                                 <td>
-                                    <span className="glyphicon glyphicon-remove remove_color" onClick={this.removeUser.bind(this,element.id)}></span>
+                                    <span className="glyphicon glyphicon-remove remove_color" onClick={this.removeUser.bind(this,element.id,element.password)}></span>
                                 </td>
                             </tr>
                         })}
